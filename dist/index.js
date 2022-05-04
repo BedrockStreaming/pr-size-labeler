0def82ced4f23fbebc03d0d03da9ac4047ee13ed
Antoine Caron <antoine.caron@bedrockstreaming.com>
2022-05-04 20:25:21 +0200

    add logs

--- dist/index.js
+++ dist/index.js
@@ -81,17 +81,51 @@ exports.default = run;
 /***/ }),
 
 /***/ 7829:
-/***/ ((__unused_webpack_module, exports, __nccwpck_require__) => {
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.getDiffSize = exports.getFileSize = exports.getPullRequest = void 0;
 const core_1 = __nccwpck_require__(2186);
-const github_1 = __nccwpck_require__(5438);
+const github = __importStar(__nccwpck_require__(5438));
 function getPullRequest() {
-    (0, core_1.info)(JSON.stringify(github_1.context));
-    (0, core_1.info)('Getting information about pull request');
+    return __awaiter(this, void 0, void 0, function* () {
+        (0, core_1.info)(JSON.stringify(github.context));
+        (0, core_1.info)('Getting information about pull request');
+        const octokit = github.getOctokit((0, core_1.getInput)('token'));
+        const { data: files } = yield octokit.rest.pulls.listFiles(Object.assign(Object.assign({}, github.context.repo), { pull_number: github.context.issue.number }));
+        (0, core_1.info)(`${files.length}`);
+        return Object.assign(Object.assign({}, github.context.payload.pull_request), { numberOfFiles: files.length });
+    });
 }
 exports.getPullRequest = getPullRequest;
 function getFileSize(configuration, pullRequest = { files: 200 }) {
--- src/pullRequest.ts
+++ src/pullRequest.ts
@@ -1,10 +1,24 @@
 import { ConfigEntry } from './config';
-import { info } from '@actions/core';
-import { context } from '@actions/github';
+import { getInput, info } from '@actions/core';
+import * as github from '@actions/github';
 
-export function getPullRequest() {
-  info(JSON.stringify(context));
+export async function getPullRequest() {
+  info(JSON.stringify(github.context));
   info('Getting information about pull request');
+
+  const octokit = github.getOctokit(getInput('token'));
+
+  const { data: files } = await octokit.rest.pulls.listFiles({
+    ...github.context.repo,
+    pull_number: github.context.issue.number,
+  });
+
+  info(`${files.length}`);
+
+  return {
+    ...github.context.payload.pull_request,
+    numberOfFiles: files.length,
+  };
 }
 
 export function getFileSize(configuration: ConfigEntry[], pullRequest = { files: 200 }): ConfigEntry {
